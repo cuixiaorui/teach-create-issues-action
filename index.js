735d3f2b82746dfc40be91b7fec983c08641b52c
cuixiaorui <cui_xiaorui@126.com>
2021-10-24 11:03:05 +0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-// github  api
-// octokit
+const { Octokit } = require("octokit");
+// nodejs12
 
 // TODO auth
-const octokit = new Octokit({ auth: `personal-access-token123` });
+const octokit = new Octokit({
+  auth: `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`,
+});
 
 // octokit api create issue
 
 octokit.rest.issues.create({
   owner: "cuixiaorui",
-  repo: "hello-world",
-  title: "Hello world from " + slug,
+  repo: "teach-create-issues-action",
+  title: "heiheihei 2 ",
+  body:"hahaha"
 });
