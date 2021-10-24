09ba09e801c9fd18553d4c2ec7465c91fcd656ef
cuixiaorui <cui_xiaorui@126.com>
2021-10-24 11:24:15 +0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Octokit } = require("octokit");
 const core = require("@actions/core");
+const dayjs = require("dayjs");
 // nodejs12
 
 const token = core.getInput("token");
@@ -12,6 +13,16 @@ const octokit = new Octokit({
 octokit.rest.issues.create({
   owner: "cuixiaorui",
   repo: "teach-create-issues-action",
-  title: "heiheihei 2 ",
-  body: "hahaha",
+  title: getTitle(),
+  body: getBody(),
 });
+
+// YYYY-MM-DD
+
+function getTitle() {
+  return dayjs().format("YYYY-MM-DD");
+}
+
+function getBody() {
+  return "[如何写每日任务](https://github.com/cuixiaorui/study-every-day/blob/main/sed/daily-task.md)";
+}
